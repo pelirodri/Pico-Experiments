9c4a8ca1327153ccbd0a8b5715d213c23f15fbb0
pelirodri <pelirodri@icloud.com>
2023-12-06 22:40:27 -0300

    feat(passive_buzzer_experiment): add better debouncing to solve erratic presses

--- passive_buzzer_experiment/src/main.c
+++ passive_buzzer_experiment/src/main.c
@@ -3,19 +3,19 @@
 
 #include <stdlib.h>
 #include <hardware/gpio.h>
-#include <hardware/timer.h>
+#include <pico/time.h>
 
 static const uint passive_buzzer_gpio_pin = 15;
 static const uint push_button_gpio_pin = 16;
 
 static const passive_buzzer_t *passive_buzzer = NULL;
+static volatile uint8_t button_pressed_history = 0;
 static volatile bool should_play_melody = false;
 
-static uint32_t interrupt_debounce_delay_ms = 20;
-static uint32_t last_interrupt_time_ms = 0;
-
 static void set_up_button();
-static void button_pressed_handler(uint, uint32_t);
+static bool button_check_callback(struct repeating_timer *);
+static void update_button_pressed_history(bool);
+static bool check_button_pressed_state();
 static int check_for_should_play_melody();
 static void clean_up();
 
@@ -27,8 +27,11 @@ int main() {
 	}
 
 	set_up_button();
+
+	struct repeating_timer timer;
+	add_repeating_timer_ms(5, button_check_callback, NULL, &timer);
 	
-	bool return_code = 0;
+	int return_code = 0;
 
 	while (true) {
 		return_code = check_for_should_play_melody();
@@ -47,22 +50,37 @@ void set_up_button() {
 	gpio_init(push_button_gpio_pin);
 	gpio_set_dir(push_button_gpio_pin, GPIO_IN);
 	gpio_pull_up(push_button_gpio_pin);
-	gpio_set_irq_enabled_with_callback(push_button_gpio_pin, GPIO_IRQ_EDGE_FALL, true, &button_pressed_handler);
 }
 
-void button_pressed_handler(uint gpio, uint32_t events) {
-	float current_time_ms = (float)time_us_32() / 1000;
+bool button_check_callback(struct repeating_timer *timer) {
+	update_button_pressed_history(!gpio_get(push_button_gpio_pin));
 
-	if (current_time_ms - last_interrupt_time_ms >= interrupt_debounce_delay_ms) {
-		if (!passive_buzzer_is_playing(passive_buzzer)) {
-			should_play_melody = true;
-		} else {
-			passive_buzzer_stop((passive_buzzer_t *)passive_buzzer);
-			should_play_melody = false;
-		}
+	if (!check_button_pressed_state()) {
+		return true;
 	}
 
-	last_interrupt_time_ms = current_time_ms;
+	if (!passive_buzzer_is_playing(passive_buzzer)) {
+		should_play_melody = true;
+	} else {
+		passive_buzzer_stop((passive_buzzer_t *)passive_buzzer);
+		should_play_melody = false;
+	}
+
+	return true;
+}
+
+inline void update_button_pressed_history(bool button_pressed_state) {
+	button_pressed_history = (button_pressed_history << 1) | button_pressed_state;
+}
+
+bool check_button_pressed_state() {
+	if ((button_pressed_history & 0xC7) != 0x7) {
+		return false;
+	}
+
+	button_pressed_history = 0xFF;
+
+	return true;
 }
 
 int check_for_should_play_melody() {
