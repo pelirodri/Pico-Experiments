2345853b5c39c9bcfd4d57e07637614ee199cd0a
pelirodri <pelirodri@icloud.com>
2024-09-06 19:12:20 -0400

    fix(passive_buzzer_experiment): fix button press detection
    I hadn’t used my Raspberry Pico W in months, and now the same code has a
    different behavior regarding the button, so I find myself forced to make
    a change… Also, I deleted some unnecessary code I seem to have glossed
    over before.

--- passive_buzzer_experiment/src/main.c
+++ passive_buzzer_experiment/src/main.c
@@ -17,7 +17,6 @@ static bool button_check_callback(struct repeating_timer *);
 static void update_button_pressed_history(bool);
 static bool check_button_pressed_state();
 static int check_for_should_play_melody();
-static void clean_up();
 
 int main() {
 	passive_buzzer = passive_buzzer_create(passive_buzzer_gpio_pin);
@@ -41,7 +40,7 @@ int main() {
 		}		
 	}
 
-	clean_up();	
+	free((passive_buzzer_t *)passive_buzzer);
 
 	return return_code;
 }
@@ -49,11 +48,10 @@ int main() {
 void set_up_button() {
 	gpio_init(push_button_gpio_pin);
 	gpio_set_dir(push_button_gpio_pin, GPIO_IN);
-	gpio_pull_up(push_button_gpio_pin);
 }
 
 bool button_check_callback(struct repeating_timer *timer) {
-	update_button_pressed_history(!gpio_get(push_button_gpio_pin));
+	update_button_pressed_history(gpio_get(push_button_gpio_pin));
 
 	if (!check_button_pressed_state()) {
 		return true;
@@ -95,9 +93,4 @@ int check_for_should_play_melody() {
 	}
 
 	return 0;
-}
-
-void clean_up() {
-	free((passive_buzzer_t *)passive_buzzer);
-	gpio_set_irq_enabled_with_callback(push_button_gpio_pin, GPIO_IRQ_EDGE_FALL, false, NULL);
 }
